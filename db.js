ccd98a7c05d2dc0f68772b2008b2456869adb24b
Prashant Kaushik <prashant.kaushik@techugo.com>
2025-02-18 22:34:02 +0530

    add database

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Data Base Connected Successfully');
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
+module.exports = connectDB;
